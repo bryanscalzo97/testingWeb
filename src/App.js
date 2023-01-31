1a41d733882bd9dea39f1dff433e30b4214fae47
Bryan Scalzo <bryan@chronosnetwork.io>
2023-01-31 16:47:04 -0300

    fixing home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+
+
 function App() {
- 
-  const handleClose = () => {
-    console.log('holaa')
-    window.postMessage("closeWebView", "*");
+  const handleBackButton = () => {
+    window.postMessage('back');
   };
   return (
      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
       <p>Example Page</p>
-      <button onClick={handleClose}>Close WebView</button>
+      <button onClick={handleBackButton}>Close WebView</button>
     </div>
   );
 }
