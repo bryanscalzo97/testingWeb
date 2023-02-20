51248097597dc52def500f4957ae83bb0d6d58e6
Bryan Scalzo <bryan@chronosnetwork.io>
2023-02-20 14:43:22 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
  import React, {useEffect, useState} from "react";
   import axios from "axios";
-  const url = require('url');
  function App() {
-  
     const [id, setId] = useState('')
-    function getAllUrlParams(urlString) {
-      // Parseamos la URL para obtener el objeto URL
-      const parsedUrl = url.parse(urlString, true);
-    
-      // Obtenemos los parámetros de la cadena de consulta
-      const queryParams = parsedUrl.query;
-    
-      // Creamos un objeto para almacenar los parámetros
-      const params = {};
-    
-      // Iteramos sobre los parámetros y los agregamos al objeto
-      for (const [key, value] of Object.entries(queryParams)) {
-        if (key.endsWith('[]')) {
-          // Si el nombre del parámetro termina en "[]", lo agregamos como un array
-          const paramKey = key.slice(0, -2);
-          params[paramKey] = Array.isArray(value) ? value : [value];
-        } else {
-          // De lo contrario, lo agregamos como un valor único
-          params[key] = value;
-        }
-      }
-    
-      return params;
-    }
+    //  function getAllUrlParams(url) {
+
+    //   // get query string from url (optional) or window
+    //   var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+    //   // we'll store the parameters here
+    //   var obj = {};
+
+    //   // if query string exists
+    //   if (queryString) {
+
+    //     // stuff after # is not part of query string, so get rid of it
+    //     queryString = queryString.split('#')[0];
+
+    //     // split our query string into its component parts
+    //     var arr = queryString.split('&');
+
+    //     for (var i = 0; i < arr.length; i++) {
+    //       // separate the keys and the values
+    //       var a = arr[i].split('=');
+
+    //       // set parameter name and value (use 'true' if empty)
+    //       var paramName = a[0];
+    //       var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+    //       // (optional) keep case consistent
+    //       paramName = paramName.toLowerCase();
+    //       if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+    //       // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+    //       if (paramName.match(/\[(\d+)?\]$/)) {
+
+    //         // create key if it doesn't exist
+    //         var key = paramName.replace(/\[(\d+)?\]/, '');
+    //         if (!obj[key]) obj[key] = [];
+
+    //         // if it's an indexed array e.g. colors[2]
+    //         if (paramName.match(/\[\d+\]$/)) {
+    //           // get the index value and add the entry at the appropriate position
+    //           var index = /\[(\d+)\]/.exec(paramName)[1];
+    //           obj[key][index] = paramValue;
+    //         } else {
+    //           // otherwise add the value to the end of the array
+    //           obj[key].push(paramValue);
+    //         }
+    //       } else {
+    //         // we're dealing with a string
+    //         if (!obj[paramName]) {
+    //           // if it doesn't exist, create property
+    //           obj[paramName] = paramValue;
+    //         } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+    //           // if property does exist and it's a string, convert it to an array
+    //           obj[paramName] = [obj[paramName]];
+    //           obj[paramName].push(paramValue);
+    //         } else {
+    //           // otherwise add the property
+    //           obj[paramName].push(paramValue);
+    //         }
+    //       }
+    //     }
+    //   }
 
     //   return obj;
     //  }
@@ -53,8 +88,8 @@
      useEffect(() => {
           const urlParams = new URLSearchParams(window.location.search)
           const queryString = urlParams.toString()
-          const result = getAllUrlParams(queryString).result;
-          setId(result)
+          setId(queryString)
+        //  const result = getAllUrlParams(urlParams).result;
         // // Imprimimos los parámetros en la consola
         // console.log('customerInternalId:', customerInternalId);
         // console.log(urlParams);
