421ec84dda188e53d703003db024d3674e7a918f
Bryan Scalzo <bryan@chronosnetwork.io>
2023-02-21 17:05:20 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,86 +1,21 @@
  import React, {useEffect} from "react";
   import axios from "axios";
  function App() {
-    //  function getAllUrlParams(url) {
-
-    //   // get query string from url (optional) or window
-    //   var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-    //   // we'll store the parameters here
-    //   var obj = {};
-
-    //   // if query string exists
-    //   if (queryString) {
-
-    //     // stuff after # is not part of query string, so get rid of it
-    //     queryString = queryString.split('#')[0];
-
-    //     // split our query string into its component parts
-    //     var arr = queryString.split('&');
-
-    //     for (var i = 0; i < arr.length; i++) {
-    //       // separate the keys and the values
-    //       var a = arr[i].split('=');
-
-    //       // set parameter name and value (use 'true' if empty)
-    //       var paramName = a[0];
-    //       var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-    //       // (optional) keep case consistent
-    //       paramName = paramName.toLowerCase();
-    //       if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-
-    //       // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-    //       if (paramName.match(/\[(\d+)?\]$/)) {
-
-    //         // create key if it doesn't exist
-    //         var key = paramName.replace(/\[(\d+)?\]/, '');
-    //         if (!obj[key]) obj[key] = [];
-
-    //         // if it's an indexed array e.g. colors[2]
-    //         if (paramName.match(/\[\d+\]$/)) {
-    //           // get the index value and add the entry at the appropriate position
-    //           var index = /\[(\d+)\]/.exec(paramName)[1];
-    //           obj[key][index] = paramValue;
-    //         } else {
-    //           // otherwise add the value to the end of the array
-    //           obj[key].push(paramValue);
-    //         }
-    //       } else {
-    //         // we're dealing with a string
-    //         if (!obj[paramName]) {
-    //           // if it doesn't exist, create property
-    //           obj[paramName] = paramValue;
-    //         } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-    //           // if property does exist and it's a string, convert it to an array
-    //           obj[paramName] = [obj[paramName]];
-    //           obj[paramName].push(paramValue);
-    //         } else {
-    //           // otherwise add the property
-    //           obj[paramName].push(paramValue);
-    //         }
-    //       }
-    //     }
-    //   }
-
-    //   return obj;
-    //  }
    async function sendData (customerInternalId, result) {
      try {
        const _datos = {
           "customerInternalId": customerInternalId,
-          //"customerInternalId": "JArAxu",
           "result": result
-         //"result": "success"
        }
        const url = 'https://nftminter-production-4518.up.railway.app/api/v1/bookings/payment'
         await axios.post(url, _datos, )
       //  setResp(mssg)
      } catch (error) {
        console.error(error)
+       window.ReactNativeWebView.postMessage('error');
        throw error
      } finally {
-         window.ReactNativeWebView.postMessage('back');
+       window.ReactNativeWebView.postMessage('back');
      }
    }
    
@@ -88,21 +23,12 @@
           const urlParams = new URLSearchParams(window.location.search)
           const customerInternalId = urlParams.get('customerInternalId');
           const result = urlParams.get('result');
-          // setId(customerId)
-        //  const result = getAllUrlParams(urlParams).result;
-        // // Imprimimos los parámetros en la consola
-        // console.log('customerInternalId:', customerInternalId);
-        // console.log(urlParams);
-        // console.log('result:', result);
-          sendData(customerInternalId, result)
-         //sendData()
-      // window.ReactNativeWebView.postMessage('back');
-        
+          sendData(customerInternalId, result)        
      }, [])
 
    return (
       <div style={{ display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: '#080029', height: '100vh' }}>
-       <p style={{color: 'white'}}>Volviendo a Eventlyy... </p>
+       <p style={{color: 'white'}}>Volviendo a Evently... </p>
      </div>
    );
  }
